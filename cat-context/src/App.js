6cc114decb8b96b2faca1e0fa3285cca8821d8f1
Joseph Kim <josephkim570@gmail.com>
2020-04-08 14:43:35 -0400

    Starter code - Added Demo Component that handles dynamic changes from input field with an assigned value

---
+++ cat-context/src/App.js
@@ -0,0 +1,32 @@
+import React, { useReducer } from 'react';
+import { StateProvider } from './context';
+import DemoComponent from './components/DemoComponent';
+
+function App() {
+  const initialState = {
+    name: "Joseph",
+  };
+
+  const reducer = (state, action) => {
+    const { name } = action;
+    switch (action.type) {
+      case 'CHANGE_NAME':
+        return {
+          ...state,
+          name
+        }
+      default:
+        return state;
+    }
+  }
+
+  return (
+    <div className="App">
+      <StateProvider value={useReducer(reducer, initialState)}>
+      <DemoComponent/>
+      </StateProvider>
+    </div>
+  );
+}
+
+export default App;
